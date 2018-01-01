a8c208f16fc268c7cf2565b1bb861123a3f428f0
Jun <junoh421@gmail.com>
2018-01-01 18:02:20 -0500

    fix message controller

--- controllers/messages_controller.js
+++ controllers/messages_controller.js
@@ -4,32 +4,24 @@ const Conversation = require('../models/conversation');
 
 module.exports = {
   sendReply(req, res, next) {
-    const username = req.body.username;
-    const conversationId = req.body.conversation_id;
+    const userId = req.body.userId;
+    const conversationId = req.body.conversationId;
     const content = req.body.content;
-    const password = req.body.password;
 
-    User.findOne({userName: username}, function(err, user) {
-      if (!user) {
-        res.status(422).send({ error: 'Username does not exist' });
+    const reply = new Message({
+      conversationId: conversationId,
+      content: content,
+      user: userId
+    });
+
+    reply.save(function(err, sentReply) {
+      if (!sentReply) {
+        res.status(422).send({ error: 'Conversation does not exist' });
         return next();
       }
 
-      const reply = new Message({
-        conversationId: conversationId,
-        content: content,
-        user: user._id
-      });
-
-      reply.save(function(err, sentReply) {
-        if (!sentReply) {
-          res.status(422).send({ error: 'Conversation does not exist' });
-          return next();
-        }
-
-        res.status(200).json({ message: 'Reply successfully sent!' });
-        return(next);
-      })
+      res.status(200).json({ message: 'Reply successfully sent!' });
+      return(next);
     })
   }
 }
